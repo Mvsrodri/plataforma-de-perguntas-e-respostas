7a18814110ad0acca5700e99c36540551f4b0b4f
Mvsrodri <mvsrodri@hotmail.com>
2023-09-25 20:39:16 -0300

    Express Router
    Criando um diretório router para tratar exclusivamente as rotas do projeto, tornando o código menos poluído.

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express =  require("express"); // importando express
 const app = express(); // iniciando o express
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
-const Pergunta = require("./database/Pergunta");
-const Resposta = require("./database/Resposta");
+
 //Database
 
 connection
@@ -20,70 +19,11 @@ app.use(express.static('public'));
 //body Parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-//Rotas
-app.get("/",(req,res)=>{
 
-    Pergunta.findAll({raw:true, order:[
-        ['id','DESC']
-    ]}).then(perguntas =>{
-        res.render("index",{
-            perguntas: perguntas
-        });
-    })
-    
-});
+//apontando as rotas
+app.use('/', require("./routes/routes"));
 
-app.get("/perguntar",(req,res)=>{
-    res.render("perguntar");
-});
 
-app.post("/salvarpergunta",(req,res)=>{
-    var titulo = req.body.titulo;
-    var descricao = req.body.descricao;
-    Pergunta.create({
-        titulo: titulo,
-        descricao: descricao
-    }).then(()=>{
-        console.log("Pergunta registrada com sucesso!!")
-        res.redirect("/");
-    });
-});
-
-app.get("/pergunta/:id",(req,res)=>{
-    var id = req.params.id;
-
-    Pergunta.findOne({
-        where: {id:id}
-    }).then(pergunta =>{
-        if(pergunta != undefined){
-            Resposta.findAll({
-                where:{perguntaId: pergunta.id},
-                order:[
-                    ['id','DESC']
-                ]
-            }).then(respostas =>{
-                res.render("pergunta",{
-                    pergunta:pergunta,
-                    respostas:respostas
-                });
-            });    
-        }else{
-            res.redirect("/");
-        }
-    });
-});
-
-app.post("/responder",(req,res)=>{
-    var corpo = req.body.corpo;
-    var perguntaId = req.body.idPergunta;
-    Resposta.create({
-        corpo: corpo,
-        perguntaId: perguntaId
-    }).then(()=>{
-        console.log("Resposta registrada com sucesso!!")
-        res.redirect("/pergunta/"+perguntaId);
-    });
-});
 
 app.listen(8080, ()=>{
     console.log("Running...");
---
+++ routes/routes.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+
+const Pergunta = require("../database/Pergunta");
+const Resposta = require("../database/Resposta");
+
+router.get("/",(req,res)=>{
+
+    Pergunta.findAll({raw:true, order:[
+        ['id','DESC']
+    ]}).then(perguntas =>{
+        res.render("index",{
+            perguntas: perguntas
+        });
+    })
+    
+});
+
+router.get("/perguntar",(req,res)=>{
+    res.render("perguntar");
+});
+
+router.post("/salvarpergunta",(req,res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(()=>{
+        console.log("Pergunta registrada com sucesso!!")
+        res.redirect("/");
+    });
+});
+
+router.get("/pergunta/:id",(req,res)=>{
+    var id = req.params.id;
+
+    Pergunta.findOne({
+        where: {id:id}
+    }).then(pergunta =>{
+        if(pergunta != undefined){
+            Resposta.findAll({
+                where:{perguntaId: pergunta.id},
+                order:[
+                    ['id','DESC']
+                ]
+            }).then(respostas =>{
+                res.render("pergunta",{
+                    pergunta:pergunta,
+                    respostas:respostas
+                });
+            });    
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+router.post("/responder",(req,res)=>{
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.idPergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(()=>{
+        console.log("Resposta registrada com sucesso!!")
+        res.redirect("/pergunta/"+perguntaId);
+    });
+});
+
+module.exports = router;
